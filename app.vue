4a8a8946bd4a97e4b9601ee5b83e879a097ab1ae
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2023-12-04 17:55:09 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div style="font-size: 80px; font-family: 'Roboto';">teste</div>
+  <div style="font-size: 80px; font-family: 'Ubuntu';">teste</div>
+  <div style="font-size: 80px;">teste</div>
+</template>
