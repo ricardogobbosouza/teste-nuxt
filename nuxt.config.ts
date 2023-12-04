addd09d347068c15aed1e9072135f9fdd13c541f
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2023-12-04 18:42:00 -0300

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/google-fonts", "@nuxtjs/tailwindcss"],
   googleFonts: {
     families: {
+      Roboto: true,
       Ubuntu: true
     }
   },
