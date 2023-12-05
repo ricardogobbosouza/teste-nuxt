154e1e247882f54ce51229eeb6ad6d3a3b4ea29e
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2023-12-05 09:47:55 -0300

    wip

--- modules/a.ts
+++ modules/a.ts
@@ -115,12 +115,16 @@ export default defineNuxtModule<ModuleOptions>({
         await downloader.execute()
 
         if (options.inject) {
+          console.log('-----------------------')
+          console.log('-----------------------')
           console.log(resolve(outputDir, options.stylePath))
           console.log(await resolvePath(resolve(outputDir, options.stylePath)))
+          console.log(await resolvePath('@/fonts/style.css'))
           console.log('-----------------------')
-          
-          nuxt.options.css.push(resolve(outputDir, options.stylePath))
-          //nuxt.options.css.push(await resolvePath('@/fonts/style.css'))
+          console.log('-----------------------')
+
+          //nuxt.options.css.push(resolve(outputDir, options.stylePath))
+          nuxt.options.css.push(await resolvePath('@/fonts/style.css'))
         }
 
         // Add the nuxt google fonts directory
