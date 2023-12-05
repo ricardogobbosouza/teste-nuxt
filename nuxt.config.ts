efb29a7d56f503ecd419ce7207f9ec5f5f9f1efb
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2023-12-05 05:51:18 -0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,9 +13,9 @@ export default defineNuxtConfig({
       routes: ['/']
     }
   },
-  cssnano: {
+  cssnano: 
     process.env.NODE_ENV === 'production'
           ? { preset: ['default', { discardComments: { removeAll: true } }] }
           : false, // disable cssnano when not in production
-  }
+  
 })
